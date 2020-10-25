3161ecfc14ebc6f816ea48f735543ef5a45ab821
Stella <stella.dmt@gmail.com>
2020-10-25 17:32:20 +0100

    Updates

---
+++ precache-manifest.61be1ebb811cbb6027e7d20fea638e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3471fdd000ee76329c6feb257d392918",
+    "url": "/api-simpsons-quotes/index.html"
+  },
+  {
+    "revision": "35df538c0f6d4b83a0ed",
+    "url": "/api-simpsons-quotes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8e14a4cb1a729b2e3cf",
+    "url": "/api-simpsons-quotes/static/js/2.1f348512.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/api-simpsons-quotes/static/js/2.1f348512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35df538c0f6d4b83a0ed",
+    "url": "/api-simpsons-quotes/static/js/main.f3375664.chunk.js"
+  },
+  {
+    "revision": "10562ab1161e078119df",
+    "url": "/api-simpsons-quotes/static/js/runtime-main.f94b5a0e.js"
+  }
+]);
